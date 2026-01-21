c7b5d9f9a84ff25161d9a0fef4b952b389eb2d91
WebXpansion <contact@webxpansion.com>
2026-01-21 10:50:32 +0100

    feat: new AR restaurant menu system

--- src/ui/menu.js
+++ src/ui/menu.js
@@ -43,9 +43,14 @@ export function renderMenu(category = 'all') {
     `
 
     if (entry) {
-      const check = document.createElement('img')
-      check.src = '/images/check.svg'
+      const check = document.createElement('span')
       check.className = 'card-check'
+      check.innerHTML = `
+      <svg viewBox="0 0 24 24" width="16" height="16" xmlns="http://www.w3.org/2000/svg">
+        <path d="M23.6103 3.45613C24.1299 3.9952 24.1299 4.866 23.6103 5.40507L8.95378 20.5957C8.42082 21.1348 7.5814 21.1348 7.06176 20.5957L0.399722 13.6846C-0.133241 13.1317 -0.133241 12.2609 0.399722 11.7218C0.919361 11.1689 1.75878 11.1689 2.27842 11.7218L7.99445 17.6516L21.7049 3.41467C22.2246 2.86178 23.064 2.86178 23.5836 3.41467Z"/>
+      </svg>
+      `
+
       card.querySelector('.info').appendChild(check)
     }
 
